a1378e0d4555dbbd812b963de5be1721b4ddc457
Long Nguyen <e0013276@u.nus.edu>
2016-11-06 20:12:04 +0700

    Add POST for update_research_work

--- service_design_toolkit/service_design_toolkit-web/src/main/java/rest/touchpoint/UpdateResearchWork.java
+++ service_design_toolkit/service_design_toolkit-web/src/main/java/rest/touchpoint/UpdateResearchWork.java
@@ -13,10 +13,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import journey.dto.TouchPointFieldResearcherDTO;
@@ -46,11 +46,10 @@ public class UpdateResearchWork {
      * Retrieves representation of an instance of rest.touchpoint.UpdateResearchWork
      * @return an instance of journey.dto.TouchPointFieldResearcherDTO
      */
-    @GET
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public TouchPointFieldResearcherDTO getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    public Response postJson(TouchPointFieldResearcherDTO content) throws AppException, CustomReasonPhraseException {
+        return Utils.buildResponse(Response.Status.CREATED, touchPointService.saveResponse(content)); 
     }
 
     /**
