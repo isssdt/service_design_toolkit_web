355f6c80fb1898626e850909dd4c44da0569e6d9
Long Nguyen <e0013276@u.nus.edu>
2016-11-06 19:15:39 +0700

    Add POST for get_touch_point_list_of_registered_journey

--- service_design_toolkit/service_design_toolkit-web/src/main/java/rest/touchpoint/GetTouchPointListOfRegisteredJourney.java
+++ service_design_toolkit/service_design_toolkit-web/src/main/java/rest/touchpoint/GetTouchPointListOfRegisteredJourney.java
@@ -15,11 +15,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import journey.dto.JourneyDTO;
 import touchpoint.ejb.business.TouchPointServiceLocal;
 import user.dto.SdtUserDTO;
 
@@ -64,10 +63,13 @@ public class GetTouchPointListOfRegisteredJourney {
      * @param journeyDTO
      * @return 
      */
-    @PUT
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void putJson(JourneyDTO journeyDTO) {
-        
+    public Response putJson(SdtUserDTO sdtUserDTO) throws AppException, CustomReasonPhraseException {
+        return Response.status(200)
+				.entity(touchPointService.getTouchPointListOfRegisteredJourney(sdtUserDTO), new Annotation[0])
+				.header("Access-Control-Allow-Headers", "X-extra-header")
+				.allow("OPTIONS").build();     
     }
 }
