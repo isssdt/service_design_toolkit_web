23f76e3c6282451046663c7eb9da80f9adf83fb9
Long Nguyen <e0013276@u.nus.edu>
2016-12-15 21:45:40 +0700

    TASK 463

--- service_design_toolkit/service_design_toolkit-ejb/src/test/java/journey/ejb/business/JourneyServiceTest.java
+++ service_design_toolkit/service_design_toolkit-ejb/src/test/java/journey/ejb/business/JourneyServiceTest.java
@@ -20,7 +20,6 @@ import journey.ejb.eao.TouchPointFacadeLocal;
 import journey.ejb.eao.TouchPointFieldResearcherFacadeLocal;
 import journey.entity.Channel;
 import journey.entity.Journey;
-import journey.entity.JourneyFieldResearcher;
 import journey.entity.Rating;
 import journey.entity.TouchPoint;
 import journey.entity.TouchpointFieldResearcher;
@@ -31,6 +30,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
+import user.dto.SdtUserDTO;
 import user.ejb.eao.SdtUserFacadeLocal;
 import user.entity.FieldResearcher;
 import user.entity.SdtUser;
@@ -233,5 +233,96 @@ public class JourneyServiceTest {
                 touchPointFieldResearcherDTO1.getFieldResearcherDTO().getSdtUserDTO().getUsername());
         Assert.assertEquals(touchpointFieldResearcher2.getFieldResearcherId().getSdtUser().getUsername(), 
                 touchPointFieldResearcherDTO2.getFieldResearcherDTO().getSdtUserDTO().getUsername());
+        
+        //latitude of those DTO should be equal to the one of entity
+        Assert.assertEquals(touchpointFieldResearcher1.getFieldResearcherId().getCurrentLatitude(), 
+                touchPointFieldResearcherDTO1.getFieldResearcherDTO().getCurrentLatitude());
+        Assert.assertEquals(touchpointFieldResearcher2.getFieldResearcherId().getCurrentLatitude(), 
+                touchPointFieldResearcherDTO2.getFieldResearcherDTO().getCurrentLatitude());  
+    }
+    
+    @Test
+    public void testGetTouchPointFiedlResearcherListByJourneyNameAndUsername() {
+        //create 2 Rating
+        Rating rating1 = new Rating(1);
+        rating1.setValue("1");
+        
+        Rating rating2 = new Rating(2);
+        rating2.setValue("2");
+        
+        //create 2 TouchPoint
+        TouchPoint touchPoint1 = new TouchPoint(1);
+        touchPoint1.setTouchPointDesc("1");
+        
+        TouchPoint touchPoint2 = new TouchPoint(2);
+        touchPoint2.setTouchPointDesc("2");
+        
+        //create 2 FieldResearcher
+        FieldResearcher fieldResearcher1 = new FieldResearcher(1);
+        fieldResearcher1.setCurrentLatitude("1");            
+        
+        FieldResearcher fieldResearcher2 = new FieldResearcher(2);
+        fieldResearcher2.setCurrentLatitude("2");        
+        
+        //create 2 TouchPointFieldResearcher
+        TouchpointFieldResearcher touchpointFieldResearcher1 = new TouchpointFieldResearcher(1);
+        touchpointFieldResearcher1.setComments("1");
+        touchpointFieldResearcher1.setFieldResearcherId(fieldResearcher1);
+        touchpointFieldResearcher1.setRatingId(rating1);
+        touchpointFieldResearcher1.setTouchpointId(touchPoint1);
+        
+        TouchpointFieldResearcher touchpointFieldResearcher2 = new TouchpointFieldResearcher(2);
+        touchpointFieldResearcher2.setComments("2");
+        touchpointFieldResearcher2.setFieldResearcherId(fieldResearcher2);
+        touchpointFieldResearcher2.setRatingId(rating2);
+        touchpointFieldResearcher2.setTouchpointId(touchPoint2);     
+        
+        //add those 2 TouchPointFieldResearcher to list
+        List<TouchpointFieldResearcher> touchpointFieldResearcherList = new ArrayList<>();
+        touchpointFieldResearcherList.add(touchpointFieldResearcher1);
+        touchpointFieldResearcherList.add(touchpointFieldResearcher2);
+        
+        //create a JourneyDTO
+        JourneyDTO journeyDTO = new JourneyDTO();
+        journeyDTO.setJourneyName("NUS Bus");
+        
+        //create a SdtUsesDTO
+        SdtUserDTO sdtUserDTO = new SdtUserDTO();
+        sdtUserDTO.setUsername("Long Nguyen");
+        
+        //mocking the findByJourneyName of TouchPointFieldResearcherFacadeLocal
+        Mockito.when(touchPointFieldResearcherFacade.findByJourneyNameAndUsername(journeyDTO, sdtUserDTO)).thenReturn(touchpointFieldResearcherList);
+        
+        //call getTouchPointFiedlResearcherListOfJourney method
+        List<TouchPointFieldResearcherDTO> touchPointFieldResearcherDTOList = 
+                journeyService.getTouchPointFiedlResearcherListByJourneyNameAndUsername(journeyDTO, sdtUserDTO);
+        
+        //size of TouchPointFieldResearcherDTOList should be equal to size of TouchPointFieldResearcherList
+        Assert.assertEquals(touchpointFieldResearcherList.size(), touchPointFieldResearcherDTOList.size());
+        
+        //get 2 DTO from TouchPointFieldResearcherDTOList
+        TouchPointFieldResearcherDTO touchPointFieldResearcherDTO1 = touchPointFieldResearcherDTOList.get(0);
+        TouchPointFieldResearcherDTO touchPointFieldResearcherDTO2 = touchPointFieldResearcherDTOList.get(1);
+        
+        //comment of those DTO should be equal to the one of entity
+        Assert.assertEquals(touchpointFieldResearcher1.getComments(), touchPointFieldResearcherDTO1.getComments());
+        Assert.assertEquals(touchpointFieldResearcher2.getComments(), touchPointFieldResearcherDTO2.getComments());
+        
+        //rating of those DTO should be equal to the one of entity
+        Assert.assertEquals(touchpointFieldResearcher1.getRatingId().getValue(), touchPointFieldResearcherDTO1.getRatingDTO().getValue());
+        Assert.assertEquals(touchpointFieldResearcher2.getRatingId().getValue(), touchPointFieldResearcherDTO2.getRatingDTO().getValue());
+        
+        //Touch Point description of those DTO should be equal to the one of entity
+        Assert.assertEquals(touchpointFieldResearcher1.getTouchpointId().getTouchPointDesc(), 
+                touchPointFieldResearcherDTO1.getTouchpointDTO().getTouchPointDesc());
+        Assert.assertEquals(touchpointFieldResearcher2.getTouchpointId().getTouchPointDesc(), 
+                touchPointFieldResearcherDTO2.getTouchpointDTO().getTouchPointDesc());
+        
+        //latitude of those DTO should be equal to the one of entity
+        Assert.assertEquals(touchpointFieldResearcher1.getFieldResearcherId().getCurrentLatitude(), 
+                touchPointFieldResearcherDTO1.getFieldResearcherDTO().getCurrentLatitude());
+        Assert.assertEquals(touchpointFieldResearcher2.getFieldResearcherId().getCurrentLatitude(), 
+                touchPointFieldResearcherDTO2.getFieldResearcherDTO().getCurrentLatitude());        
+        
     }
 }
