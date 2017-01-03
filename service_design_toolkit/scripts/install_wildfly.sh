87791214054798e8c603fdf17ea8a64944da2c8c
Long Nguyen <e0013276@u.nus.edu>
2017-01-03 22:52:58 +0700

    Update install wildfly script.

--- service_design_toolkit/scripts/install_wildfly.sh
+++ service_design_toolkit/scripts/install_wildfly.sh
@@ -21,7 +21,7 @@ wget http://download.jboss.org/wildfly/9.0.1.Final/wildfly-9.0.1.Final.zip
 sudo unzip wildfly-9.0.1.Final.zip -d /opt/
 
 # Wildfly defaults config
-sudo cp /opt/wildfly/bin/init.d/wildfly9.conf /etc/default/wildfly9
+sudo cp /opt/wildfly-9.0.1.Final/bin/init.d/wildfly.conf /etc/default/wildfly9
 # Changes to configure:
 sudo sed -i 's|# JAVA_HOME="/usr/lib/jvm/default-java"|JAVA_HOME="/usr/lib/jvm/java-8-oracle"|g' /etc/default/wildfly9
 sudo sed -i 's|# JBOSS_HOME="/opt/wildfly"|JBOSS_HOME="/opt/wildfly-9.0.1.Final"|g' /etc/default/wildfly9
@@ -33,7 +33,7 @@ sudo sed -i 's|# SHUTDOWN_WAIT=60|SHUTDOWN_WAIT=60|g' /etc/default/wildfly9
 sudo sed -i 's|# JBOSS_CONSOLE_LOG="/var/log/wildfly/console.log"|JBOSS_CONSOLE_LOG="/var/log/wildfly/console.log"|g' /etc/default/wildfly9
 
 # Wildfly Init file
-sudo cp /opt/wildfly/bin/init.d/wildfly-init-debian.sh /etc/init.d/wildfly9
+sudo cp /opt/wildfly-9.0.1.Final/bin/init.d/wildfly-init-debian.sh /etc/init.d/wildfly9
 sudo chown root:root /etc/init.d/wildfly9
 sudo chmod +x /etc/init.d/wildfly9
 # Changes in Init
@@ -49,5 +49,8 @@ sudo chown -R wildfly:wildfly /var/log/wildfly
 
 # Tunning
 sudo sed -i 's|   JAVA_OPTS="-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"|   JAVA_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=256m -XX:NewRatio=2 -XX:PermSize=64m -Djava.net.preferIPv4Stack=true"|g' /opt/wildfly-9.0.1.Final/bin/standalone.conf
+
+sudo update-rc.d wildfly9 defaults
+sudo update-rc.d wildfly9 enable
 
 sudo service wildfly9 status
